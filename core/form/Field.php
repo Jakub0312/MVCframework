9a5b07e2967d40817bf462cc19301f9c6651eceb
Jakub Belczyk <9017270@student.zadkine.nl>
2022-05-30 11:07:58 +0200

    part 2

--- core/form/Field.php
+++ core/form/Field.php
@@ -6,11 +6,17 @@ use app\core\Model;
 
 class Field
 {
+    public const TYPE_TEXT = 'text';
+    public const TYPE_PASSWORD = 'password';
+    public const TYPE_NUMBER = 'number';
+
+    public string $type;
     public Model $model;
     public string $attribute;
 
     public function __construct(\app\core\Model $model, string $attribute)
     {
+        $this->type = self::TYPE_TEXT;
         $this->model = $model;
         $this->attribute = $attribute;
     }
@@ -20,17 +26,24 @@ class Field
         return sprintf('
             <div class="form-group">
                 <label>%s</label>
-                <input type="text" name="lastname" value="%s" id="%s" class="form-control %s">
+                <input type="%s" name="lastname" value="%s" id="%s" class="form-control %s">
             <div class="invalid-feedback">
             %s
             </div>
         </div>
         ',
             $this->attribute,
+            $this->type,
             $this->model->{$this->attribute},
             $this->attribute,
             $this->model->hasError($this->attribute) ? ' is-invalid' : '',
             $this->model->getFirstError($this->attribute)
         );
+    }
+
+    public function passwordField()
+    {
+        $this->type = self::TYPE_PASSWORD;
+        return $this;
     }
 }
